f6513283769b9a39ffcadf2f11ce20592426d542
Indigo253931 <indigo253931@gmail.com>
2016-06-17 16:24:42 -0600

    Merge branch 'master' of https://github.com/Indigo253931/js-functions-lab

--- starter-code/functions.js
+++ starter-code/functions.js
@@ -1,52 +1,98 @@
 // Question 1
-function maxOfTwoNumbers() {
 
+function maxOfTwoNumbers(a, b) {
+if (a > b) {
+	return a;
 }
+	else if (b > a) {
+		return b;
+	}
+
+
+	else {
+		return "They're equal.";
+	}
+}
+maxOfTwoNumbers(3, 9);
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(a, b, c) {
+if (a > b && a > c) {
+	return a;
 }
 
-// Question 3
-function isCharacterAVowel() {
+	else if (b > a && b > c) {
+		return b;
+	}
 
+else {
+	return c;
 }
+}
+
+maxOfThree(5, 8, 10);
+
+// Question 3"
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+function isCharacterAVowel(character) {
+	for (i = 0; i < vowels.length; i++)
+if (character === vowels[i]) {
+	return "true";
+	
+}
+
+	else {
+		return "false";
+		
+	}
+}
+
+isCharacterAVowel("p");
 
 // Question 4
-function sumArray() {
- 
+
+var sum = 0;
+var ary1 = [1, 2, 3, 4];
+
+function multiplyArray(numbers) {
+	for (var ary1 = 0; ary1 < ary1.length; ary1++) {
+		sum + numbers[0] + ary1[1] + ary1[2] + ary1[2]
+}
+return sum;
 }
 
-
-// Question 4
-function multiplyArray() {
-
+function sumArray(numbers) {
+	for (var i = 0; i < numbers.length; i++)
+	return i;
 }
 
 
 // Question 5
-var numberOfArguments = function(){
-  
-}
-
-
+var numberOfArguments = function(arg1, arg2, arg3){
+  for (var i = 0; i < numberOfArguments.length; i++) { 
+  	return numberOfArguments; 
+  }
+};
+var arg1=1;
+var arg2 = 2;
+var arg3 = 3;
 
 // Question 6
-var reverseString = function (){
-  
-};
-
-
+function reverseString(string) {
+    return string.split('').reverse().join('');
+}
+reverseString('hello');
 // Question 7
-function findLongestWord () {
-  
+function findLongestWord ([]) {
+  return findLonestWord;
 }
 
 
 // Question 8
-function filterLongWords () {
-  
+var i = 3;
+function filterLongWords ([a, b, c, i]) {
+  return Array.filter(filterLongWords);
 }
 
 
